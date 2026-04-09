6411bd1456bde3a6b47e3a3a62949c5e8f9116de
ETHXN <migethanu@gmail.com>
2026-04-09 10:16:06 +0100

    implement linked list delete with head and middle/tail case handling

--- linkedList.c
+++ linkedList.c
@@ -8,6 +8,7 @@ typedef struct node {
 
 void insert(node **, int value);
 void print(node *);
+void delete(node **, int);
 
 int main() {
   node *list = NULL;
@@ -15,6 +16,12 @@ int main() {
   insert(&list, 4);
   insert(&list, 5);
   insert(&list, 6);
+  insert(&list, 3);
+  insert(&list, 2);
+
+  delete(&list, 6);
+  delete(&list, 2);
+  delete(&list, 4);
 
   print(list);
 
@@ -38,4 +45,27 @@ void print(node *head) {
     printf("%d ", ptr->number);
     ptr = ptr->next;
   }
+}
+
+void delete(node **head, int target) {
+
+  node *current = *head;
+  node *previous = NULL;
+
+  while (current != NULL) {
+    if ((current)->number == target) {
+      if (previous == NULL) {
+        *head = (*head)->next;
+        free(current);
+        return;
+      } else {
+        previous->next = current->next;
+        free(current);
+        return;
+      }
+    }
+    previous = current;
+    current = current->next;
+  }
+  printf("Error: %d not found\n", target);
 }
