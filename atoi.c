1dd66070cfca504b2c8fa40200222741786bd231
ETHXN <migethanu@gmail.com>
2026-04-15 10:09:41 +0100

    implement string to int converter

---
+++ atoi.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+int stringToInt(char s[], int);
+
+int main() {
+
+  char word[] = "3657";
+  int size = strlen(word);
+
+  int num = stringToInt(word, size);
+
+  printf("%d", num);
+
+  return 0;
+}
+
+int stringToInt(char s[], int n) {
+  int num;
+
+  for (int i = 0; i < n; i++)
+    num = num * 10 + (s[i] - '0');
+
+  return num;
+}
